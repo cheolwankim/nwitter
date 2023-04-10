b4d078ff7e613280cc9ab624715e4606bb657e28
MSDN-SPECIAL\Administrator <[sam_000@naver.com]>
2023-04-10 10:34:24 +0900

    firebase.js and .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDaichXXIiobR9zV2QfcQCegxsfTPHvalM",
-    authDomain: "nwitter-94a85.firebaseapp.com",
-    projectId: "nwitter-94a85",
-    storageBucket: "nwitter-94a85.appspot.com",
-    messagingSenderId: "590745384409",
-    appId: "1:590745384409:web:cc0dd3364f58960e4e95c8"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT_APP_APP_ID
   };
   
 // const app = initializeApp(firebaseConfig);
 
 export default firebase.initializeApp(firebaseConfig)
+

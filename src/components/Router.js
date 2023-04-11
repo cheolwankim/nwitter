cb20406b9885df2a69579acc7f1416f182fb6b20
MSDN-SPECIAL\Administrator <[sam_000@naver.com]>
2023-04-11 09:25:15 +0900

    2-5..로그아웃

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,31 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>

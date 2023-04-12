154c837ee8615a30f920dd5f95a61b81d53f7413
MSDN-SPECIAL\Administrator <[sam_000@naver.com]>
2023-04-13 03:15:51 +0900

    cleanup

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,42 +1,8 @@
+import React, { useState } from "react";
 import { authService, firebaseInstance } from "fbase";
-import { useState } from "react";
+import AuthForm from "components/AuthForm";
 
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
-  const [error, setError] = useState("");
-  const onChange = (event) => {
-    //onchange 2개 동시에받기
-    const {
-      target: { name, value }, //event.target에서 key가 name, value 인것 추출
-    } = event;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let data;
-    try {
-      if (newAccount) {
-        //newAccount이 true면 creat account
-        data = await authService.createUserWithEmailAndPassword(
-          email,
-          password
-        );
-      } else {
-        //log in
-        data = await authService.signInWithEmailAndPassword(email, password);
-      }
-      console.log(data);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-  const toggleAccount = () => setNewAccount((prev) => !prev);
   const onSocialClick = async (event) => {
     const {
       target: { name },
@@ -47,34 +13,12 @@ const Auth = () => {
     } else if (name === "github") {
       provider = new firebaseInstance.auth.GithubAuthProvider();
     }
-    const data = await authService.signInWithPopup(provider);
-    console.log(data);
+
+    await authService.signInWithPopup(provider);
   };
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={onChange}
-          required
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={onChange}
-          required
-        />
-        <input type="submit" value={newAccount ? "Create Acount" : "Log In"} />
-        {error}
-      </form>
-      <span onClick={toggleAccount}>
-        {newAccount ? "Sign In" : "Create Account"}
-      </span>
+      <AuthForm />
       <div>
         <button onClick={onSocialClick} name="google">
           Continue with Google
@@ -86,5 +30,4 @@ const Auth = () => {
     </div>
   );
 };
-
 export default Auth;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,13 @@
-import Nweet from "components/Nweet";
-import { dbService, storageService } from "fbase";
 import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+
+import { dbService} from "fbase";
+import Nweet from "components/Nweet";
+import NweetFactory from "components/NweetFactory";
 
 const Home = ({ userObj }) => {
-  const [nweet, setNweet] = useState("");
+  
   const [nweets, setNweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
-
+  
   useEffect(() => {
     dbService.collection("nweets").onSnapshot((snapshot) => {
       const nweetArray = snapshot.docs.map((doc) => ({
@@ -17,80 +17,11 @@ const Home = ({ userObj }) => {
       setNweets(nweetArray);
     });
   }, []);
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService
-        .ref()
-        .child(`${userObj.uid}/${uuidv4()}`);
-      //이미지의 path를 child에
-      //파일에대한 ref 가짐
-
-      const response = await attachmentRef.putString(attachment, "data_url");
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-
-    const nweetObj = {
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-
-    await dbService.collection("nweets").add(nweetObj);
-    //submit할때마다 nweet doc 생성하기
-    setNweet("");
-    setAttachment("");
-  };
-  const onChange = (event) => {
-    const {
-      target: { value }, //event.target.value 추출
-    } = event;
-    setNweet(value);
-  };
-  const onFileChange = (event) => {
-    // console.log(event.target.files); // FileList
-    const {
-      //event.target에서 files 추출
-      target: { files },
-    } = event;
-
-    const theFile = files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = (finishedEvent) => {
-      //load end시 실행
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-
-  const onClearAttachment = () => setAttachment(null);
-
+ 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={nweet}
-          onChange={onChange}
-          placeholder="What's on Your Mind?"
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Nweet" />
-        {attachment && (
-          <div>
-            <img src={attachment} width="100px" height="100px" alt="" />
-            <button onClick={onClearAttachment}>Clear Image</button>
-          </div>
-        )}
-      </form>
+    
+      <NweetFactory userObj={userObj} />
       <div>
         {nweets.map((nweet) => (
           <Nweet
@@ -103,5 +34,4 @@ const Home = ({ userObj }) => {
     </div>
   );
 };
-
 export default Home;

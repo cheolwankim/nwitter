e4c5264f5e135a90b48bfe68e9c3ac7d91f1b42c
MSDN-SPECIAL\Administrator <[sam_000@naver.com]>
2023-04-13 02:10:03 +0900

    5-2. Profile(1)

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,10 +1,27 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const onLogOutClick = () => {
     authService.signOut();
   };
 
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createAt")
+      .get();
+    //where - 필터링함
+    console.log(nweets.docs.map((doc) => doc.data()));
+
+    // firestore noSQL 기반이라 에러나올떄 있음
+  };
+
+  useEffect(() => {
+    //Nweet function 호출
+    getMyNweets();
+  });
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
